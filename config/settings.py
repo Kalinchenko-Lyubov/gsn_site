1a726f1350a0b6da29d84d0abc14061a42121b6c
Luybov <kalna.lyubov2017@yandex.ru>
2026-07-07 15:37:16 +0300

    Fix static file paths

--- config/settings.py
+++ config/settings.py
@@ -18,11 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
+STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -125,5 +123,7 @@ from pathlib import Path
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
