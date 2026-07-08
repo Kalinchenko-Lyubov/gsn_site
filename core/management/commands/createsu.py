6d5c8343641f3440578aa9dab8bfeba823fb37a5
Luybov <kalna.lyubov2017@yandex.ru>
2026-07-08 11:35:08 +0300

    Add createsu management command

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -6,11 +6,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='',
-                password='admin123456'
-            )
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin123456')
             self.stdout.write(self.style.SUCCESS('Superuser created!'))
         else:
             self.stdout.write('Superuser already exists')
