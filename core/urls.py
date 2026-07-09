750cf33e908930d6bff150551eeff8cc53717607
Luybov <kalna.lyubov2017@yandex.ru>
2026-07-09 11:02:15 +0300

    Fix debug photos URL in config/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
-
-from core import views
-from core.views import home, projects_gallery
-
+# from django.urls import path
+# from core.views import home, projects_gallery
+#
 # urlpatterns = [
 #     path('', home, name='home'),
 #
@@ -13,8 +11,17 @@ from core.views import home, projects_gallery
 #     ),
 # ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from core import views  # Добавьте эту строку
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('projects/', views.projects_gallery, name='projects_gallery'),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('debug-photos/', views.debug_photos, name='debug_photos'),  # Добавьте эту строку
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
