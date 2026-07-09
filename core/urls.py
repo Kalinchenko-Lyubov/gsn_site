1560d65f604f4db078d9c46a739bf6b087cf1746
Luybov <kalna.lyubov2017@yandex.ru>
2026-07-09 10:59:15 +0300

    Add debug photos page

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 
+from core import views
 from core.views import home, projects_gallery
 
-urlpatterns = [
-    path('', home, name='home'),
+# urlpatterns = [
+#     path('', home, name='home'),
+#
+#     path(
+#         'projects/',
+#         projects_gallery,
+#         name='projects_gallery'
+#     ),
+# ]
 
-    path(
-        'projects/',
-        projects_gallery,
-        name='projects_gallery'
-    ),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('projects/', views.projects_gallery, name='projects_gallery'),
+    path('debug-photos/', views.debug_photos, name='debug_photos'),  # Добавьте эту строку
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 import os
 from django.shortcuts import render
 from django.conf import settings
-from core.models import Service, Project, FAQ
+from core.models import Project, FAQ
+from django.http import JsonResponse
 
 
 def projects_gallery(request):
@@ -47,3 +48,39 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+
+def debug_photos(request):
+    """Временная страница для отладки фото"""
+    result = {}
+
+    # Проверяем MEDIA_ROOT
+    result['MEDIA_ROOT'] = str(settings.MEDIA_ROOT)
+    result['MEDIA_ROOT_exists'] = os.path.exists(settings.MEDIA_ROOT)
+
+    # Проверяем projects папку
+    projects_dir = os.path.join(settings.MEDIA_ROOT, 'projects')
+    result['projects_dir'] = str(projects_dir)
+    result['projects_dir_exists'] = os.path.exists(projects_dir)
+
+    if os.path.exists(projects_dir):
+        folders = os.listdir(projects_dir)
+        result['folders_in_projects'] = folders
+
+    # Проверяем проекты из БД
+    result['projects_in_db'] = []
+    for p in Project.objects.all():
+        project_dir = os.path.join(settings.MEDIA_ROOT, 'projects', p.title)
+        project_info = {
+            'id': p.id,
+            'title': p.title,
+            'dir': str(project_dir),
+            'exists': os.path.exists(project_dir),
+        }
+        if os.path.exists(project_dir):
+            files = [f for f in os.listdir(project_dir) if
+                     f.lower().endswith(('.jpg', '.jpeg', '.png', '.webp', '.gif'))]
+            project_info['photos'] = files
+        result['projects_in_db'].append(project_info)
+
+    return JsonResponse(result, json_dumps_params={'ensure_ascii': False, 'indent': 2})
